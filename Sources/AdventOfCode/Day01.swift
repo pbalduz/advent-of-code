f58582feaf6f24868359f1340f0396f28fd840d7
Pablo Balduz <pablo.balduz@xing.com>
2023-12-01 16:45:11 +0100

    Day 1 2023

--- Sources/AdventOfCode/Day01.swift
+++ Sources/AdventOfCode/Day01.swift
@@ -1,14 +1,69 @@
-import Algorithms
-
 struct Day01: AdventDay {
     let data: String
     
     func part1() -> Any {
-        0
+        data.components(separatedBy: "\n")
+            .map {
+                let digits = $0.compactMap(\.wholeNumberValue)
+                guard
+                    let first = digits.first,
+                    let last = digits.last
+                else {
+                    return 0
+                }
+                return first * 10 + last
+            }
+            .reduce(0, +)
     }
-    
-    // Replace this with your solution for the second part of the day's challenge.
+
     func part2() -> Any {
-        0
+        data.components(separatedBy: "\n")
+            .map { line in
+                var numbers = [Int]()
+                for index in line.indices {
+                    if let number = line[index].wholeNumberValue {
+                        numbers.append(number)
+                        continue
+                    }
+                    for number in Numbers.allCases {
+                        guard line.range(of: number.rawValue, range: index..<line.endIndex)?.lowerBound == index else {
+                            continue
+                        }
+                        numbers.append(number.value)
+                        break
+                    }
+                }
+
+                let first = numbers.first ?? 0
+                let last = numbers.last ?? 0
+                return first * 10 + last
+            }
+            .reduce(0, +)
+    }
+}
+
+private enum Numbers: String, CaseIterable {
+    case one
+    case two
+    case three
+    case four
+    case five
+    case six
+    case seven
+    case eight
+    case nine
+
+    var value: Int {
+        switch self {
+        case .one: 1
+        case .two: 2
+        case .three: 3
+        case .four: 4
+        case .five: 5
+        case .six: 6
+        case .seven: 7
+        case .eight: 8
+        case .nine: 9
+        }
     }
 }
--- Tests/AdventOfCodeTests/Day01Tests.swift
+++ Tests/AdventOfCodeTests/Day01Tests.swift
@@ -2,17 +2,30 @@ import XCTest
 @testable import AdventOfCode
 
 final class Day00Tests: XCTestCase {
-    // Smoke test data provided in the challenge question
-    let testData = """
+    let part1Data = """
+    1abc2
+    pqr3stu8vwx
+    a1b2c3d4e5f
+    treb7uchet
+    """
+
+    let part2Data = """
+    two1nine
+    eightwothree
+    abcone2threexyz
+    xtwone3four
+    4nineeightseven2
+    zoneight234
+    7pqrstsixteen
     """
 
     func testPart1() throws {
-        let challenge = Day01(data: testData)
-        XCTAssertEqual(String(describing: challenge.part1()), "0")
+        let challenge = Day01(data: part1Data)
+        XCTAssertEqual(String(describing: challenge.part1()), "142")
     }
 
     func testPart2() throws {
-        let challenge = Day01(data: testData)
-        XCTAssertEqual(String(describing: challenge.part2()), "0")
+        let challenge = Day01(data: part2Data)
+        XCTAssertEqual(String(describing: challenge.part2()), "281")
     }
 }
