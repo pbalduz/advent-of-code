9897d01ca78a370ee6b0261b29de3901097521c1
Pablo Balduz <9513953+pbalduz@users.noreply.github.com>
2023-12-07 19:08:06 +0100

    Day 3 2023

---
+++ Tests/AdventOfCodeTests/Day03Tests.swift
@@ -0,0 +1,27 @@
+import XCTest
+@testable import AdventOfCode
+
+final class Day03Tests: XCTestCase {
+    let data = """
+    467..114..
+    ...*......
+    ..35..633.
+    ......#...
+    617*......
+    .....+.58.
+    ..592.....
+    ......755.
+    ...$.*....
+    .664.598..
+    """
+
+    func testPart1() throws {
+        let challenge = Day03(data: data)
+        XCTAssertEqual(String(describing: challenge.part1()), "4361")
+    }
+
+    func testPart2() throws {
+        let challenge = Day03(data: data)
+        XCTAssertEqual(String(describing: challenge.part2()), "467835")
+    }
+}
